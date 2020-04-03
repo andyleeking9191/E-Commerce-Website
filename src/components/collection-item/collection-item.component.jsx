de8c155e79f4246a10bd2dc1fd0ec531747a52c0
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-04-02 21:36:30 -0400

    Added collection-preview and collection-item components

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './collection-item.styles.scss';
+
+const CollectionItem = ({}) => (
+    <div className='collection-item'>
+        <div
+        className='image'
+        >
+
+        </div>
+    </div>
+);
+
+export default CollectionItem;
